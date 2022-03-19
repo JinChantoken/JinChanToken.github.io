4db24b2823099064a7968c5fb5d6810a2deaa626
JinChantoken <jinchantoken@protonmail.com>
2022-03-19 11:42:09 +0000

    fix: ui and content

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-f6f6d67cda0ef43bf8bc.js
@@ -0,0 +1,2 @@
+(self.webpackChunkjin_chan=self.webpackChunkjin_chan||[]).push([[306],{1930:function(e,n,t){"use strict";var u=t(5318);n.__esModule=!0,n.default=void 0;var a=u(t(5354)),r=u(t(7294)),l=function(e){function n(){return e.apply(this,arguments)||this}return(0,a.default)(n,e),n.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},n}(r.default.Component);n.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-f6f6d67cda0ef43bf8bc.js.map
